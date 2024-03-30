1d8dceda93838112dc0198ed81fee705e763c6c0
Test Test <ani@MacBook-Pro-de-Test.local>
2024-03-30 10:57:00 -0300

    initial version of serial port

---
+++ main.js
@@ -0,0 +1,48 @@
+const SerialPort = require("serialport");
+const RestClient = require('./rest-client');
+
+const API_BASE_URL = 'http://192.168.1.13:3000/api';
+const client = new RestClient(API_BASE_URL);
+
+var portMac = '/dev/tty.usbserial-A401279N';
+var portWindows = 'COM5'
+
+const mySerial = new SerialPort(portWindows,{
+	baudRate:9600,
+	dataBits: 7,
+	stopBits: 1,
+	Parity: 'none',
+	flowControl: false,
+	buffer : 4096
+}, false)
+
+//const parser = new SerialPort.parsers.Readline()
+
+
+// when your serial port receives data, this event is fired
+// so you can capture the data and do what you need
+
+var result = ''
+mySerial.on('readable', async function() {
+	try {
+		const data = await mySerial.read(2465)
+		if(data != null){
+	
+            (async (data) => {
+                // POST request example
+                const newPost = {
+                    result : data.toString()
+                };
+                const createdPost = await client.post('/save-result/counter19', newPost);
+                console.log('POST /posts:', createdPost);
+            })();
+
+		
+		}
+	} catch (error) {
+		console.log(error)
+	}
+	
+})
+
+
